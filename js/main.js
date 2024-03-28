5e12b3533599899fd070b151b0e0cf4c09a797ab
Mateus Fuchs <mateus5fuchs@gmail.com>
2024-03-27 21:58:27 -0300

    Interface inicial para exibir imagens em canvas criada

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const maxTamanhoImagem = 400;
+let img;
+let canvas = document.getElementById('imagem-canvas');
+let ctx = canvas.getContext('2d');
+
+document.getElementById('input-imagem').addEventListener('change', function (e) {
+
+  let reader = new FileReader();
+
+  reader.onload = function (event) {
+    img = new Image();
+    img.onload = function () {
+      let razao = img.height / img.width;
+      if (img.height > maxTamanhoImagem || img.width > maxTamanhoImagem) {
+        if (razao > 1) {
+          canvas.height = maxTamanhoImagem;
+          canvas.width = maxTamanhoImagem / razao;
+        } else {
+          canvas.width = maxTamanhoImagem;
+          canvas.height = maxTamanhoImagem * razao;
+        }
+      } else {
+        canvas.width = img.width;
+        canvas.height = img.height;
+      }
+      document.getElementById('msg').hidden = true;
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
+    img.src = event.target.result;
+  };
+  reader.readAsDataURL(e.target.files[0]);
+});
